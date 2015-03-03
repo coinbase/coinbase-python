79500d534a4f1c7033224343627cf007f2187818
Ashish Bhatia <ashishbhatia.ab@gmail.com>
2015-03-02 16:57:05 -0800

    Added method call to redeem token.

--- coinbase/client.py
+++ coinbase/client.py
@@ -181,6 +181,13 @@ class Client(object):
           'Could not parse API response')
     return account
 
+  def redeem_token(self, tokenId):
+      data = encode_params({
+              'token_id': tokenId})
+      response = self._post('tokens/redeem', data=data)
+      api_obj = self._make_api_object(response.json())
+      return api_obj.get('success', False)
+
   def get_contacts(self, page=None, limit=None, all_accounts=None):
     """https://developers.coinbase.com/api#contacts"""
     data = encode_params({
